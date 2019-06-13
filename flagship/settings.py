4164da1e9fa2f3c61a4fd3018eb853bbe78db75d
Paul Laue <paul@pauljlaue.com>
2019-06-13 15:30:42 -0500

    Add Amazon S3; added django-storages; added boto3

--- flagship/settings.py
+++ flagship/settings.py
@@ -62,6 +62,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'widget_tweaks',
     'adminsortable2',
+    'storages',
     
     'api',
     'iam',
@@ -152,22 +153,35 @@ USE_L10N = True
 
 USE_TZ = True
 
+# S3 Media Storage
+if not DEBUG:
+    AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+    AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+    AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=86400',
+    }
+    AWS_LOCATION = 'static'
+    AWS_QUERYSTRING_AUTH = False
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'flagship/static'),
+    ]
+    STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    DEFAULT_FILE_STORAGE = 'flagship.storage_backends.MediaStorage'
+else:
+    # Static files (CSS, JavaScript, Images)
+    # https://docs.djangoproject.com/en/2.2/howto/static-files/
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+    ]
+    MEDIA_URL = '/media/'
+    # MEDIA_ROOT = os.path.join(os.path.join(BASE_DIR), 'media')
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
-MEDIA_URL = '/media/'
-'''
-MEDIA_ROOT = os.path.join(os.path.join(BASE_DIR), 'media')
-'''
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
